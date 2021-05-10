1d8fe13ac927ae94da04d2e379e05f0f8460f574
AJ Steers <aaronsteers@gmail.com>
2021-05-10 12:21:23 -0700

    add useful search endpoint properties

--- tap_github/streams.py
+++ tap_github/streams.py
@@ -30,15 +30,42 @@ class RepositoryStream(GitHubStream):
         th.Property("id", th.IntegerType),
         th.Property("name", th.StringType),
         th.Property("full_name", th.StringType),
+        th.Property("description", th.StringType),
         th.Property(
             "owner",
             th.ObjectType(
                 th.Property("login", th.StringType),
                 th.Property("type", th.StringType),
+                th.Property("avatar_url", th.StringType),
             ),
         ),
+        th.Property(
+            "license",
+            th.ObjectType(
+                th.Property("key", th.StringType),
+                th.Property("name", th.StringType),
+                th.Property("url", th.StringType),
+                th.Property("spdx_id", th.StringType),
+            ),
+        ),
+        th.Property("master_branch", th.StringType),
+        th.Property("default_branch", th.StringType),
         th.Property("updated_at", th.DateTimeType),
         th.Property("created_at", th.DateTimeType),
+        th.Property("pushed_at", th.DateTimeType),
+        th.Property("private", th.BooleanType),
+        th.Property("size", th.IntegerType),
+        th.Property("stargazers_count", th.IntegerType),
+        th.Property("fork", th.BooleanType),
+        # These `_count` metrics appear to be duplicates, or else
+        # documentation bugs in results example:
+        # https://docs.github.com/en/rest/reference/search
+        th.Property("forks", th.IntegerType),
+        th.Property("forks_count", th.IntegerType),
+        th.Property("watchers", th.IntegerType),
+        th.Property("watchers_count", th.IntegerType),
+        th.Property("open_issues", th.IntegerType),
+        th.Property("open_issues_count", th.IntegerType),
     ).to_dict()
 
     def get_url_params(
