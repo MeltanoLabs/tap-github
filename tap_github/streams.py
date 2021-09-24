80baee27b50d210c1b3e9eac7c498580a03bbfeb
Eric Boucher <eric.p.boucher@gmail.com>
2021-09-24 13:53:23 -0700

    Add IssueEvents stream (#19)
    Co-authored-by: Ryan Samarakoon <ryan2002.rs@gmail.com>

--- tap_github/streams.py
+++ tap_github/streams.py
@@ -446,7 +446,7 @@ class IssuesStream(GitHubStream):
 
 class IssueCommentsStream(GitHubStream):
     """
-    Defines 'Issues' stream.
+    Defines 'IssueComments' stream.
     Issue comments are fetched from the repository level (as opposed to per issue)
     to optimize for API quota usage.
     """
@@ -487,6 +487,64 @@ class IssueCommentsStream(GitHubStream):
         th.Property("body", th.StringType),
         th.Property(
             "user",
+            th.ObjectType(
+                th.Property("login", th.StringType),
+                th.Property("id", th.IntegerType),
+                th.Property("node_id", th.StringType),
+                th.Property("avatar_url", th.StringType),
+                th.Property("gravatar_id", th.StringType),
+                th.Property("html_url", th.StringType),
+                th.Property("type", th.StringType),
+                th.Property("site_admin", th.BooleanType),
+            ),
+        ),
+    ).to_dict()
+
+
+class IssueEventsStream(GitHubStream):
+    """
+    Defines 'IssueEvents' stream.
+    Issue events are fetched from the repository level (as opposed to per issue)
+    to optimize for API quota usage.
+    """
+
+    name = "issue_events"
+    path = "/repos/{org}/{repo}/issues/events"
+    primary_keys = ["id"]
+    replication_key = "created_at"
+    parent_stream_type = RepositoryStream
+    state_partitioning_keys = ["repo", "org"]
+    ignore_parent_replication_key = False
+
+    def get_records(self, context: Optional[dict] = None) -> Iterable[Dict[str, Any]]:
+        """Return a generator of row-type dictionary objects.
+
+        Each row emitted should be a dictionary of property names to their values.
+        """
+        if context and context.get("events", None) == 0:
+            self.logger.debug(f"No events detected. Skipping '{self.name}' sync.")
+            return []
+
+        return super().get_records(context)
+
+    def post_process(self, row: dict, context: Optional[dict] = None) -> dict:
+        row["issue_number"] = int(row["issue"].pop("number"))
+        row["issue_url"] = row["issue"].pop("url")
+        return row
+
+    schema = th.PropertiesList(
+        th.Property("id", th.IntegerType),
+        th.Property("node_id", th.StringType),
+        th.Property("repo", th.StringType),
+        th.Property("org", th.StringType),
+        th.Property("issue_number", th.IntegerType),
+        th.Property("issue_url", th.StringType),
+        th.Property("event", th.StringType),
+        th.Property("commit_id", th.StringType),
+        th.Property("commit_url", th.StringType),
+        th.Property("created_at", th.DateTimeType),
+        th.Property(
+            "actor",
             th.ObjectType(
                 th.Property("login", th.StringType),
                 th.Property("id", th.IntegerType),
