b72ffbac123c767f4c1325db4aca0d275deecfed
Eric Boucher <eric.p.boucher@gmail.com>
2021-11-25 17:54:10 +0100

    Add as new stream

--- tap_github/streams.py
+++ tap_github/streams.py
@@ -162,6 +162,42 @@ class RepositoryStream(GitHubStream):
     ).to_dict()
 
 
+class ReadmeStream(GitHubStream):
+    name = "readme"
+    path = "/repos/{org}/{repo}/readme"
+    primary_keys = ["repo", "org"]
+    parent_stream_type = RepositoryStream
+    ignore_parent_replication_key = False
+    state_partitioning_keys = ["repo", "org"]
+    tolerated_http_errors = [404]
+
+    schema = th.PropertiesList(
+        # Parent Keys
+        th.Property("repo", th.StringType),
+        th.Property("org", th.StringType),
+        # README Keys
+        th.Property("type", th.StringType),
+        th.Property("encoding", th.StringType),
+        th.Property("size", th.IntegerType),
+        th.Property("name", th.StringType),
+        th.Property("path", th.StringType),
+        th.Property("content", th.StringType),
+        th.Property("sha", th.StringType),
+        th.Property("url", th.StringType),
+        th.Property("git_url", th.StringType),
+        th.Property("html_url", th.StringType),
+        th.Property("download_url", th.StringType),
+        th.Property(
+            "_links",
+            th.ObjectType(
+                th.Property("git", th.StringType),
+                th.Property("self", th.StringType),
+                th.Property("html", th.StringType),
+            ),
+        ),
+    ).to_dict()
+
+
 class ReadmeHtmlStream(GitHubStream):
     name = "readme"
     path = "/repos/{org}/{repo}/readme"
