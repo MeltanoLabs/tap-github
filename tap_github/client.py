47904c99111d71c80a24013ea7e7e177e673393c
Eric Boucher <eric.p.boucher@gmail.com>
2022-02-09 23:33:16 +0100

    Update tap_github/client.py
    Co-authored-by: Laurent Savaete <laurent@where.tf>

--- tap_github/client.py
+++ tap_github/client.py
@@ -117,7 +117,7 @@ class GitHubRestStream(RESTStream):
 
         if self.replication_key == "updated_at":
             params["sort"] = "updated"
-            params["direction"] = "asc" if not self.missing_since_parameter else "desc"
+            params["direction"] = "desc" if self.missing_since_parameter else "asc"
 
         # Unfortunately the /starred, /stargazers (starred_at) and /events (created_at) endpoints do not support
         # the "since" parameter out of the box. But we use a workaround in 'get_next_page_token'.
