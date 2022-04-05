709f1be42282417fa12265a4b28db8ab4fb5f4ba
Eric Boucher <eric.p.boucher@gmail.com>
2022-04-05 16:18:58 -0400

    Add ORG_LEVEL_TOKEN input

--- tap_github/tests/test_core.py
+++ tap_github/tests/test_core.py
@@ -1,4 +1,8 @@
 """Tests standard tap features using the built-in SDK tests library."""
+import os
+import logging
+
+from unittest import mock
 
 from singer_sdk.testing import get_standard_tap_tests
 
@@ -33,12 +37,15 @@ def test_standard_tap_tests_for_username_list_mode(username_list_config):
     for test in tests:
         test()
 
-
+# This token needs to have read:org access for the organization listed in fixtures.py
+# Default is "MeltanoLabs"
+ORG_LEVEL_TOKEN = os.environ.get('ORG_LEVEL_TOKEN')
+@mock.patch.dict(os.environ, {"GITHUB_TOKEN": ORG_LEVEL_TOKEN or ""})
 def test_standard_tap_tests_for_organization_list_mode(organization_list_config):
     """Run standard tap tests from the SDK."""
+    if not ORG_LEVEL_TOKEN:
+        logging.warning('No "ORG_LEVEL_TOKEN" found. Skipping organization tap tests.')
+        return
     tests = get_standard_tap_tests(TapGitHub, config=organization_list_config)
     for test in tests:
         test()
-
-
-# TODO: Create additional tests as appropriate for your tap.
--- tap_github/tests/test_tap.py
+++ tap_github/tests/test_tap.py
@@ -1,6 +1,13 @@
+import os
+import logging
 import pytest
 
+from unittest.mock import patch
+
+from singer_sdk.streams.core import Stream
+
 from tap_github.tap import TapGitHub
+from tap_github.tests.test_core import ORG_LEVEL_TOKEN
 
 from .fixtures import repo_list_config
 
@@ -22,6 +29,28 @@ def test_validate_repo_list_config(repo_list_config):
     assert partitions == repo_list_context
 
 
+# TODO - patch mock sync_children to use ORG_LEVEL_TOKEN for the "collaborators stream"
+def mock_sync_chidren(self, child_context: dict) -> None:
+        for child_stream in self.child_streams:
+            # Use org:read access level credentials for collaborators stream
+            if child_stream.name in ["collaborators"]:
+                ORG_LEVEL_TOKEN = os.environ.get('ORG_LEVEL_TOKEN')
+                if not ORG_LEVEL_TOKEN:
+                    logging.warning('No "ORG_LEVEL_TOKEN" found. Skipping collaborators stream sync.')
+                    continue
+                SAVED_GTHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+                os.environ['GITHUB_TOKEN'] = ORG_LEVEL_TOKEN
+                child_stream.sync(context=child_context)
+                os.environ['GITHUB_TOKEN'] = SAVED_GTHUB_TOKEN or ""
+                continue
+            
+            # default behavior:
+            if child_stream.selected or child_stream.has_selected_descendents:
+                child_stream.sync(context=child_context)
+
+                
+# Mock the update method
+# @patch.object(Stream, '_sync_children')
 @pytest.mark.repo_list(repo_list_2)
 def test_get_a_repository_in_repo_list_mode(capsys, repo_list_config):
     """
