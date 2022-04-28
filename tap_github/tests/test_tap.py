e88548c70098956863b69c7ec1da28ce64694b42
Laurent Savaete <laurent@where.tf>
2022-04-28 11:37:17 +0300

    Expand tests to check case correction and user streams

--- tap_github/tests/test_tap.py
+++ tap_github/tests/test_tap.py
@@ -6,12 +6,14 @@ from unittest.mock import patch
 
 from tap_github.tap import TapGitHub
 
-from .fixtures import repo_list_config
+from .fixtures import repo_list_config, username_list_config
 
 repo_list_2 = [
     "MeltanoLabs/tap-github",
-    "MeltanoLabs/tap-gitlab",
-    "MeltanoLabs/target-athena",
+    # mistype the repo name so we can check that the tap corrects it
+    "MeltanoLabs/Tap-GitLab",
+    # mistype the org
+    "meltanolabs/target-athena",
 ]
 # the github repo ids that match the repo names above
 # in the same order
@@ -31,7 +33,7 @@ def test_validate_repo_list_config(repo_list_config):
             "repo": repo[0].split("/")[1],
             "repo_id": repo[1],
         }
-        for repo in zip(repo_list_2, repo_ids)
+        for repo in zip(repo_list_2, repo_list_2_ids)
     ]
     tap = TapGitHub(config=repo_list_config)
     partitions = tap.streams["repositories"].partitions
@@ -63,29 +65,53 @@ def alternative_sync_chidren(self, child_context: dict) -> None:
             child_stream.sync(context=child_context)
 
 
-@pytest.mark.repo_list(repo_list_2)
-def test_get_a_repository_in_repo_list_mode(capsys, repo_list_config):
+def run_tap_with_config(capsys, config_obj) -> str:
     """
-    Discover the catalog, and request 2 repository records
+    Run the tap with the given config and capture stdout
     """
-    tap1 = TapGitHub(config=repo_list_config)
+    tap1 = TapGitHub(config=config_obj)
     tap1.run_discovery()
     catalog = tap1.catalog_dict
-    # disable child streams
-    # FIXME: this does not work, the child streams are still fetched
-    # deselect_all_streams(catalog)
-    # set_catalog_stream_selected(
-    #     catalog=catalog, stream_name="repositories", selected=True
-    # )
     # discard previous output to stdout (potentially from other tests)
     capsys.readouterr()
     with patch(
         "singer_sdk.streams.core.Stream._sync_children", alternative_sync_chidren
     ):
-        tap2 = TapGitHub(config=repo_list_config, catalog=catalog)
+        tap2 = TapGitHub(config=config_obj, catalog=catalog)
         tap2.sync_all()
     captured = capsys.readouterr()
+    return captured.out
+
+
+@pytest.mark.repo_list(repo_list_2)
+def test_get_a_repository_in_repo_list_mode(capsys, repo_list_config):
+    """
+    Discover the catalog, and request 2 repository records
+    """
+    captured_out = run_tap_with_config(capsys, repo_list_config)
     # Verify we got the right number of records (one per repo in the list)
-    assert captured.out.count('{"type": "RECORD", "stream": "repositories"') == len(
+    assert captured_out.count('{"type": "RECORD", "stream": "repositories"') == len(
         repo_list_2
     )
+    # check that the tap corrects invalid case in config input
+    assert '"repo": "Tap-GitLab"' not in captured_out
+    assert '"org": "meltanolabs"' not in captured_out
+
+
+# case is incorrect on purpose, so we can check that the tap corrects it
+@pytest.mark.repo_list(["EricBoucher", "aaRONsTeeRS"])
+def test_get_a_user_in_user_usernames_mode(capsys, username_list_config):
+    """
+    Discover the catalog, and request 2 repository records
+    """
+    captured_out = run_tap_with_config(capsys, username_list_config)
+    # Verify we got the right number of records (one per user in the list)
+    assert captured_out.count('{"type": "RECORD", "stream": "users"') == len(
+        username_list_config["user_usernames"]
+    )
+    # these 2 are inequalities as number will keep changing :)
+    assert captured_out.count('{"type": "RECORD", "stream": "starred"') > 150
+    assert captured_out.count('{"type": "RECORD", "stream": "user_contributed_to"') > 50
+    assert '{"username": "aaronsteers"' in captured_out
+    assert '{"username": "aaRONsTeeRS"' not in captured_out
+    assert '{"username": "EricBoucher"' not in captured_out
