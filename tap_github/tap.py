ba69209135f4566b48b3bcb7e67808461436596a
Eric Boucher <eric.p.boucher@gmail.com>
2022-05-20 15:50:08 -0400

    Update tap.py

--- tap_github/tap.py
+++ tap_github/tap.py
@@ -25,17 +25,12 @@ class TapGitHub(Tap):
             Logger with local LOGLEVEL. LOGLEVEL from env takes priority.
         """
 
-        class NoParsingFilter(logging.Filter):
-            def filter(self, record):
-                return not " was present in the " in record.getMessage()
-
         LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
         assert (
             LOGLEVEL in logging._levelToName.values()
         ), f"Invalid LOGLEVEL configuration: {LOGLEVEL}"
         logger = logging.getLogger(cls.name)
         logger.setLevel(LOGLEVEL)
-        logger.addFilter(NoParsingFilter())
         return logger
 
     config_jsonschema = th.PropertiesList(
